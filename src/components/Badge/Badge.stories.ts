d7255250a9af238a2fb0514c734578110f949593
Laurens Groeneveld <laurens.groeneveld@bcc.no>
2023-03-03 13:26:20 +0100

    Add badge component (#16)

---
+++ src/components/Badge/Badge.stories.ts
@@ -0,0 +1,104 @@
+import Badge from "./Badge.vue";
+import UncheckedRadio from "@icons/radio_button_unchecked.svg";
+
+import type { Meta, StoryFn } from "@storybook/vue3";
+
+export default {
+  title: "Components/Badge",
+  component: Badge,
+  argTypes: {
+    type: {
+      description: "Determines the styling of the badge",
+      options: ["neutral", "error", "warning", "success", "info", "systemInfo"],
+      control: { type: "radio" },
+    },
+    size: {
+      description: "Size of the badge",
+      options: ["base", "lg"],
+      control: { type: "radio" },
+    },
+    iconPosition: {
+      description:
+        "On which side the contents of the icon slot are rendered",
+      options: ["left", "right"],
+      control: { type: "radio" },
+    },
+    slotDefault: {
+      name: "default slot",
+      description: "The badge text",
+    },
+  },
+} as Meta<typeof Badge>;
+
+const Template: StoryFn<typeof Badge> = (args) => ({
+  components: { Badge, UncheckedRadio },
+  setup() {
+    return { args };
+  },
+  template: `
+    <Badge v-bind="args">
+      <template #icon>
+        <UncheckedRadio />
+      </template>
+      <template #default>
+        {{ args.slotDefault }}
+      </template>
+    </Badge>
+  `,
+});
+
+export const Example = Template.bind({});
+Example.parameters = {
+  viewMode: "docs",
+};
+Example.args = {
+  type: "neutral",
+  size: "base",
+  iconPosition: "left",
+  slotDefault: "Example Badge",
+};
+
+export const Types: StoryFn<typeof Badge> = () => ({
+  components: { Badge },
+  template: `
+    <div class="flex items-start space-x-2">
+      <Badge type="neutral">neutral</Badge>
+      <Badge type="error">error</Badge>
+      <Badge type="warning">warning</Badge>
+      <Badge type="success">success</Badge>
+      <Badge type="info">info</Badge>
+    </div>
+  `,
+});
+
+export const WithIcon: StoryFn<typeof Badge> = () => ({
+  components: { Badge, UncheckedRadio },
+  template: `
+    <div class="flex items-start space-x-2">
+      <Badge type="error" size="base">
+        <template #icon>
+          <UncheckedRadio />
+        </template>
+        base, icon left
+      </Badge>
+      <Badge type="error" size="base" iconPosition="right">
+        <template #icon>
+          <UncheckedRadio />
+        </template>
+        base, icon right
+      </Badge>
+      <Badge type="success" size="lg">
+        <template #icon>
+          <UncheckedRadio />
+        </template>
+        lg, icon left
+      </Badge>
+      <Badge type="success" size="lg" iconPosition="right">
+        <template #icon>
+          <UncheckedRadio />
+        </template>
+        lg, icon right
+      </Badge>
+    </div>
+  `,
+});
---
+++ src/components/Badge/Badge.vue
@@ -0,0 +1,64 @@
+<script setup lang="ts">
+import { cva, type VariantProps } from "class-variance-authority";
+
+const badgeClassVariants = cva("py-0.5 rounded-md space-x-1 inline-flex items-center", {
+  variants: {
+    type: {
+      neutral: "bg-neutral-50 text-neutral-900 dark:bg-neutral-900 dark:text-neutral-300",
+      error: "bg-red-50 text-red-900 dark:bg-red-900 dark:text-red-300",
+      warning: "bg-yellow-50 text-yellow-900 dark:bg-yellow-900 dark:text-yellow-300",
+      success: "bg-green-50 text-green-900 dark:bg-green-900 dark:text-green-300",
+      info: "bg-blue-50 text-blue-900 dark:bg-blue-900 dark:text-blue-300",
+      systemInfo: "bg-teal-50 text-teal-900 dark:bg-teal-900 dark:text-teal-300",
+    },
+    size: {
+      "base": "text-xs px-2.5",
+      "lg": "text-sm px-3 leading-5",
+    },
+    iconPosition: {
+      left: "",
+      right: "flex-row-reverse space-x-reverse",
+    },
+  },
+});
+
+type BadgeVariants = VariantProps<typeof badgeClassVariants>;
+
+const iconClassVariants = cva("inline-block", {
+  variants: {
+    type: {
+      neutral: "fill-neutral-800",
+      error: "fill-red-800",
+      warning: "fill-yellow-800",
+      success: "fill-green-800",
+      info: "fill-blue-800",
+      systemInfo: "fill-teal-800",
+    },
+    size: {
+      "base": "w-2.5 h-2.5",
+      "lg": "w-4 h-4",
+    },
+  },
+});
+
+type Props = {
+  type?: BadgeVariants["type"];
+  size?: BadgeVariants["size"];
+  iconPosition?: BadgeVariants["iconPosition"];
+};
+
+withDefaults(defineProps<Props>(), {
+  type: "neutral",
+  size: "base",
+  iconPosition: "left",
+});
+</script>
+
+<template>
+  <span :class="badgeClassVariants({ type, size, iconPosition })">
+    <span :class="iconClassVariants({ type, size })" v-if="$slots.icon">
+      <slot name="icon"></slot>
+    </span>
+    <span><slot /></span>
+  </span>
+</template>
