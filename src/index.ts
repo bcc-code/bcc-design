4edc399a620476cf0fd20a52838bfe7b5ac92bdd
Laurens Groeneveld <laurens.groeneveld@bcc.no>
2023-03-04 12:20:03 +0100

    Add missing export for Badge

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export { default as BccButton } from "./components/BccButton/BccButton.vue";
 export { default as BccInput } from "./components/BccInput/BccInput.vue";
+export { default as Badge } from "./components/Badge/Badge.vue";
