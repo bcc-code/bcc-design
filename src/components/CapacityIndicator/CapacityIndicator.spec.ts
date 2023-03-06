178ba9de34896dd7d1c0f82076354ab149201894
Laurens Groeneveld <laurens.groeneveld@bcc.no>
2023-03-06 15:34:01 +0100

    Add unit test

--- src/components/CapacityIndicator/CapacityIndicator.spec.ts
+++ src/components/CapacityIndicator/CapacityIndicator.spec.ts
@@ -4,5 +4,8 @@ import { mount } from "@vue/test-utils";
 import CapacityIndicator from "./CapacityIndicator.vue";
 
 describe("CapacityIndicator", () => {
-
+  it("show the remaining capacity", () => {
+    const wrapper = mount(CapacityIndicator, { props: { capacity: 20, left: 14 } });
+    expect(wrapper.text()).toBe("14");
+  });
 });
