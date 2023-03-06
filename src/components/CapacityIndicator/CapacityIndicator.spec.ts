30a9b5a66d3d4cabb1b90d548242a9a95d5df8b7
Laurens Groeneveld <laurens.groeneveld@bcc.no>
2023-03-06 17:04:51 +0100

    Fix tests

--- src/components/CapacityIndicator/CapacityIndicator.spec.ts
+++ src/components/CapacityIndicator/CapacityIndicator.spec.ts
@@ -1,11 +1,17 @@
 import { describe, it, expect } from "vitest";
+import { AllInclusiveIcon } from "@bcc-code/icons-vue";
 
 import { mount } from "@vue/test-utils";
 import CapacityIndicator from "./CapacityIndicator.vue";
 
 describe("CapacityIndicator", () => {
-  it("show the remaining capacity", () => {
-    const wrapper = mount(CapacityIndicator, { props: { capacity: 20, left: 14 } });
-    expect(wrapper.text()).toBe("14");
+  it("shows the remaining capacity", () => {
+    const wrapper = mount(CapacityIndicator, { props: { capacity: 20, used: 14 } });
+    expect(wrapper.text()).toBe("6");
+  });
+
+  it("shows an icon if the capacity is infinite", () => {
+    const wrapper = mount(CapacityIndicator, { props: { capacity: Infinity, used: 14 } });
+    expect(wrapper.findComponent(AllInclusiveIcon).isVisible()).toBe(true);
   });
 });
