1b7ee82e4b4a797935e6a803627ae9c68468d52d
Laurens Groeneveld <laurens.groeneveld@bcc.no>
2023-03-07 16:01:26 +0100

    Add test for optional used prop

--- src/components/CapacityIndicator/CapacityIndicator.spec.ts
+++ src/components/CapacityIndicator/CapacityIndicator.spec.ts
@@ -9,8 +9,13 @@ describe("CapacityIndicator", () => {
     expect(wrapper.text()).toBe("6");
   });
 
+  it("shows all capacity without the used prop", () => {
+    const wrapper = mount(CapacityIndicator, { props: { total: 42 } });
+    expect(wrapper.text()).toBe("42");
+  });
+
   it("shows an icon if the capacity is infinite", () => {
-    const wrapper = mount(CapacityIndicator, { props: { total: Infinity } });
+    const wrapper = mount(CapacityIndicator, { props: { total: -1 } });
     expect(wrapper.html()).toContain("<path");
   });
 });
