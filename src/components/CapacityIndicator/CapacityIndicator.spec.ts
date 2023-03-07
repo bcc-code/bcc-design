e0ea00476893aa61d6080fb556b9d397e739e5f9
Laurens Groeneveld <laurens.groeneveld@bcc.no>
2023-03-07 16:36:39 +0100

    Refactor capacity indicator (#25)
    * Make `used` prop optional
    * Bring it back to 40x40
    * Use colors from design

--- src/components/CapacityIndicator/CapacityIndicator.spec.ts
+++ src/components/CapacityIndicator/CapacityIndicator.spec.ts
@@ -8,4 +8,14 @@ describe("CapacityIndicator", () => {
     const wrapper = mount(CapacityIndicator, { props: { total: 20, used: 14 } });
     expect(wrapper.text()).toBe("6");
   });
+
+  it("shows all capacity without the used prop", () => {
+    const wrapper = mount(CapacityIndicator, { props: { total: 42 } });
+    expect(wrapper.text()).toBe("42");
+  });
+
+  it("shows an icon if the capacity is infinite", () => {
+    const wrapper = mount(CapacityIndicator, { props: { total: -1 } });
+    expect(wrapper.html()).toContain("<path");
+  });
 });
