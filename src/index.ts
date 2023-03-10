ef783a489f2e2c82bf818bec1f108f18e85cb388
Laurens Groeneveld <laurens.groeneveld@bcc.no>
2023-03-10 14:11:14 +0100

    Actually rename exports

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export { default as BccButton } from "./components/BccButton/BccButton.vue";
 export { default as BccInput } from "./components/BccInput/BccInput.vue";
-export { default as Badge } from "./components/BccBadge/BccBadge.vue";
-export { default as CapacityIndicator } from "./components/BccCapacityIndicator/BccCapacityIndicator.vue";
+export { default as BccBadge } from "./components/BccBadge/BccBadge.vue";
+export { default as BccCapacityIndicator } from "./components/BccCapacityIndicator/BccCapacityIndicator.vue";
