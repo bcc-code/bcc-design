9b376cf7dcb8d518772248ffc8bbc18cc1e27887
Laurens Groeneveld <laurens.groeneveld@bcc.no>
2023-03-13 12:34:42 +0100

    Don't add slot to stories by default

--- scripts/templates/create-stories-file.sh
+++ scripts/templates/create-stories-file.sh
@@ -6,10 +6,6 @@ export default {
   title: \"Components/$1\",
   component: $1,
   argTypes: {
-    slotDefault: {
-      name: \"default slot\",
-      description: \"\",
-    },
   },
 } as Meta<typeof $1>;
 
@@ -19,11 +15,7 @@ const Template: StoryFn<typeof $1> = (args) => ({
     return { args };
   },
   template: \`
-    <$1 v-bind=\"args\">
-      <template #default>
-        {{ args.slotDefault }}
-      </template>
-    </$1>
+    <$1 v-bind=\"args\" />
   \`,
 });
 
@@ -32,6 +24,5 @@ Example.parameters = {
   viewMode: \"docs\",
 };
 Example.args = {
-  slotDefault: \"Example $1\",
 };
 " > src/components/$1/$1.stories.ts
