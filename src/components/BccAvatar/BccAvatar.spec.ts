ae693bd2629aad9a4cfaa1306db9aaa4c61582c6
Laurens Groeneveld <laurens.groeneveld@bcc.no>
2023-03-13 10:10:44 +0100

    Add tests

--- src/components/BccAvatar/BccAvatar.spec.ts
+++ src/components/BccAvatar/BccAvatar.spec.ts
@@ -4,5 +4,13 @@ import { mount } from "@vue/test-utils";
 import BccAvatar from "./BccAvatar.vue";
 
 describe("BccAvatar", () => {
+  it("renders the initials", () => {
+    const wrapper = mount(BccAvatar, { props: { initials: "LG" } });
+    expect(wrapper.text()).toBe("LG");
+  });
 
+  it("renders only the first two letters of the initials", () => {
+    const wrapper = mount(BccAvatar, { props: { initials: "Frodo" } });
+    expect(wrapper.text()).toBe("Fr");
+  });
 });
