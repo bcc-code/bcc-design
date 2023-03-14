10ab546941072aab7241ec4ad614df7711095ede
Gerard <gerardlamo@gmail.com>
2023-03-14 15:58:33 +0100

    Add test for pin

--- src/components/BccPin/BccPin.spec.ts
+++ src/components/BccPin/BccPin.spec.ts
@@ -2,7 +2,18 @@ import { describe, it, expect } from "vitest";
 
 import { mount } from "@vue/test-utils";
 import BccPin from "./BccPin.vue";
+import { CheckCircleIcon } from "@bcc-code/icons-vue";
 
 describe("BccPin", () => {
+  it("renders a pin", () => {
+    expect(BccPin).toBeTruthy();
 
+    const wrapper = mount(BccPin, {
+      props: {
+        icon: CheckCircleIcon,
+      },
+    });
+
+    expect(wrapper.html()).toMatchSnapshot();
+  });
 });
