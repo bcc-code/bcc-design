86d7f7fdbf15d22b3938c30cf1c45b43c54d4128
Laurens Groeneveld <laurens.groeneveld@bcc.no>
2023-03-15 18:38:44 +0100

    Update snapshot

--- src/components/BccCapacityIndicator/BccCapacityIndicator.vue
+++ src/components/BccCapacityIndicator/BccCapacityIndicator.vue
@@ -13,14 +13,16 @@ const props = withDefaults(
   }
 );
 
-const { value: valueUsed } = useAnimatedNumber(toRef(props, 'used'))
+const { value: valueUsed } = useAnimatedNumber(toRef(props, "used"));
 
-const { value: progress } = useAnimatedNumber(computed(() => {
-  if (props.total === -1) return -1;
-  if (props.used > props.total) return 100;
-  if (props.total > 0) return (props.used / props.total) * 100;
-  return 0;
-}))
+const { value: progress } = useAnimatedNumber(
+  computed(() => {
+    if (props.total === -1) return -1;
+    if (props.used > props.total) return 100;
+    if (props.total > 0) return (props.used / props.total) * 100;
+    return 0;
+  })
+);
 
 const dashArray = Math.PI * 18 * 2;
 </script>
