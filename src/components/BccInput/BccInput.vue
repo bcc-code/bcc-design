49962f8e0a8de6c3f6a66db338abe9b3b2449ead
Laurens Groeneveld <laurens.groeneveld@bcc.no>
2023-03-16 17:25:02 +0100

    Use tokens (#38)
    + Update 'error' to 'danger' on Badge prop
    + Make design-tokens package a dependency instead of peer/devDependency
    + move headlessui & vueuse packages to devDependencies

--- src/components/BccInput/BccInput.vue
+++ src/components/BccInput/BccInput.vue
@@ -10,9 +10,9 @@ import { cva, type VariantProps } from "class-variance-authority";
 const inputClassVariants = cva("px-4 py-3 text-sm rounded-lg border focus:outline-2", {
   variants: {
     state: {
-      default: "focus:outline-primary-dark-green-600 border-neutral-300",
-      error: "border-red-500 bg-red-50 text-red-700 focus:outline-red-600",
-      success: "border-green-500 bg-green-50 text-green-700 focus:outline-green-600",
+      default: "focus:outline-silver-tree-600 border-neutral-300",
+      error: "border-danger bg-red-50 text-danger focus:outline-red-600",
+      success: "border-success bg-green-50 text-success focus:outline-green-600",
     },
     disabled: {
       true: "cursor-not-allowed",
@@ -37,9 +37,9 @@ const inputClassVariants = cva("px-4 py-3 text-sm rounded-lg border focus:outlin
 const messageClassVariants = cva("text-sm", {
   variants: {
     state: {
-      default: "text-neutral-500",
-      error: "text-red-600",
-      success: "text-green-600",
+      default: "text-secondary",
+      error: "text-danger",
+      success: "text-success",
     },
   },
   compoundVariants: [],
--- src/composables/variants.ts
+++ src/composables/variants.ts
@@ -1,18 +1,23 @@
 export const variants = {
-  neutral: "border-neutral-200 bg-neutral-50 text-neutral-900 dark:bg-neutral-900 dark:text-neutral-200 dark:border-neutral-700",
-  error: "border-red-200 bg-red-50 text-red-900 dark:bg-red-900 dark:text-red-200 dark:border-red-700",
-  warning: "border-yellow-200 bg-yellow-50 text-yellow-900 dark:bg-yellow-900 dark:text-yellow-300 dark:border-yellow-700",
-  success: "border-green-200 bg-green-50 text-green-900 dark:bg-green-900 dark:text-green-300 dark:border-green-700",
-  info: "border-blue-200 dark bg-blue-50 text-blue-900 dark:bg-blue-900 dark:text-blue-300 dark:border-blue-700",
-  systemInfo: "border-teal-200 bg-teal-50 text-teal-900 dark:bg-teal-900 dark:text-teal-300 dark:border-teal-700",
-}
+  neutral:
+    "border-neutral-200 bg-neutral-50 text-neutral-900 dark:bg-neutral-900 dark:text-neutral-200 dark:border-neutral-700",
+  danger:
+    "border-danger bg-surface-danger-default text-danger dark:bg-red-900 dark:text-red-200 dark:border-red-700",
+  warning:
+    "border-warning bg-surface-warning-default text-warning dark:bg-yellow-900 dark:text-yellow-300 dark:border-yellow-700",
+  success:
+    "border-success bg-surface-success-default text-success dark:bg-green-900 dark:text-green-300 dark:border-green-700",
+  info: "border-info bg-surface-info-default text-info dark:bg-blue-900 dark:text-blue-300 dark:border-blue-700",
+  systemInfo:
+    "border-system bg-surface-system-default text-system dark:bg-teal-900 dark:text-teal-300 dark:border-teal-700",
+};
 
 export const sizeVariants = {
-  xxs: 'text-xxs',
-  xs: 'text-xs',
-  sm: 'text-sm',
-  md: 'text-base',
-  lg: 'text-lg',
-  xl: 'text-xl',
-  '2xl': 'text-2xl',
-}
+  xxs: "text-xxs",
+  xs: "text-xs",
+  sm: "text-sm",
+  md: "text-base",
+  lg: "text-lg",
+  xl: "text-xl",
+  "2xl": "text-2xl",
+};
