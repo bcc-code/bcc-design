3bbc53fb5eb7c3cc4c12dbe7cc2ace3266905b93
Laurens Groeneveld <laurens.groeneveld@bcc.no>
2023-03-28 15:27:53 +0200

    Add toggle from event app

---
+++ src/components/BccToggle/BccToggle.spec.ts
@@ -0,0 +1,8 @@
+import { describe, it, expect } from "vitest";
+
+import { mount } from "@vue/test-utils";
+import BccToggle from "./BccToggle.vue";
+
+describe("BccToggle", () => {
+
+});
---
+++ src/components/BccToggle/CircleLoader.vue
@@ -0,0 +1,86 @@
+<template>
+  <svg fill="none" viewBox="0 0 58 58" aria-hidden="true">
+    <g transform="translate(2 1)" stroke="currentColor" stroke-width="1.5" class="h-3 w-3">
+      <circle cx="42.601" cy="11.462" r="5" fill-opacity="1" fill="currentColor">
+        <animate
+          attributeName="fill-opacity"
+          begin="0s"
+          dur="1.3s"
+          values="1;0;0;0;0;0;0;0"
+          calcMode="linear"
+          repeatCount="indefinite"
+        />
+      </circle>
+      <circle cx="49.063" cy="27.063" r="5" fill-opacity="0" fill="currentColor">
+        <animate
+          attributeName="fill-opacity"
+          begin="0s"
+          dur="1.3s"
+          values="0;1;0;0;0;0;0;0"
+          calcMode="linear"
+          repeatCount="indefinite"
+        />
+      </circle>
+      <circle cx="42.601" cy="42.663" r="5" fill-opacity="0" fill="currentColor">
+        <animate
+          attributeName="fill-opacity"
+          begin="0s"
+          dur="1.3s"
+          values="0;0;1;0;0;0;0;0"
+          calcMode="linear"
+          repeatCount="indefinite"
+        />
+      </circle>
+      <circle cx="27" cy="49.125" r="5" fill-opacity="0" fill="currentColor">
+        <animate
+          attributeName="fill-opacity"
+          begin="0s"
+          dur="1.3s"
+          values="0;0;0;1;0;0;0;0"
+          calcMode="linear"
+          repeatCount="indefinite"
+        />
+      </circle>
+      <circle cx="11.399" cy="42.663" r="5" fill-opacity="0" fill="currentColor">
+        <animate
+          attributeName="fill-opacity"
+          begin="0s"
+          dur="1.3s"
+          values="0;0;0;0;1;0;0;0"
+          calcMode="linear"
+          repeatCount="indefinite"
+        />
+      </circle>
+      <circle cx="4.938" cy="27.063" r="5" fill-opacity="0" fill="currentColor">
+        <animate
+          attributeName="fill-opacity"
+          begin="0s"
+          dur="1.3s"
+          values="0;0;0;0;0;1;0;0"
+          calcMode="linear"
+          repeatCount="indefinite"
+        />
+      </circle>
+      <circle cx="11.399" cy="11.462" r="5" fill-opacity="0" fill="currentColor">
+        <animate
+          attributeName="fill-opacity"
+          begin="0s"
+          dur="1.3s"
+          values="0;0;0;0;0;0;1;0"
+          calcMode="linear"
+          repeatCount="indefinite"
+        />
+      </circle>
+      <circle cx="27" cy="5" r="5" fill-opacity="0" fill="currentColor">
+        <animate
+          attributeName="fill-opacity"
+          begin="0s"
+          dur="1.3s"
+          values="0;0;0;0;0;0;0;1"
+          calcMode="linear"
+          repeatCount="indefinite"
+        />
+      </circle>
+    </g>
+  </svg>
+</template>
