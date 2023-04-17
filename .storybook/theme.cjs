15be4c5c48bbf322f3ef809376d27270cbbf6941
Laurens Groeneveld <laurens.groeneveld@bcc.no>
2023-04-17 17:37:08 +0200

    Always use light theme in Storybook

--- .storybook/theme.cjs
+++ .storybook/theme.cjs
@@ -3,4 +3,5 @@ import { create } from '@storybook/theming';
 export default create({
   brandTitle: 'BCC Vue Components',
   brandImage: 'https://design.bcc.no/logos/bcc_logo_primary.svg',
+  base: "light",
 });
