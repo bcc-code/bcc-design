bbc055e64228695c65da4abf6df5b1567f6f2909
Laurens Groeneveld <laurens.groeneveld@bcc.no>
2023-04-17 17:21:37 +0200

    Disable dark mode in Storybook

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,5 +4,6 @@ const themes = require("@bcc-code/design-tokens");
 module.exports = {
   content: ["./src/**/*.{vue,ts}"],
   theme: themes.bccForbundetTheme,
+  darkMode: 'class', // To disable dark mode, see https://developer.bcc.no/bcc-design/vue-components/#disable-dark-mode
   plugins: [],
 };
