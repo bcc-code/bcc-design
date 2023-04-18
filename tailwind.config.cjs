af44a81d9c705b393eef4f038711e3870f4d332e
Laurens Groeneveld <laurens.groeneveld@bcc.no>
2023-04-18 17:28:04 +0200

    Update to latest token package

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,9 @@ const themes = require("@bcc-code/design-tokens");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{vue,ts}"],
-  theme: themes.bccForbundetTheme,
+  presets: [
+    themes.bccForbundetTheme
+  ],
   darkMode: 'class', // To disable dark mode, see https://developer.bcc.no/bcc-design/vue-components/#disable-dark-mode
   plugins: [],
 };
