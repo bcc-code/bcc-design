c293cc01eb6d5e9d176c31f0864ac93bee53b834
Laurens Groeneveld <laurens.groeneveld@bcc.no>
2023-04-20 16:01:09 +0200

    Revert leftover changes to TW config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,5 +1,4 @@
 const themes = require("@bcc-code/design-tokens");
-const plugin = require('tailwindcss/plugin')
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
@@ -8,4 +7,5 @@ module.exports = {
     themes.bccForbundetTheme
   ],
   darkMode: 'class', // To disable dark mode, see https://developer.bcc.no/bcc-design/vue-components/#disable-dark-mode
+  plugins: []
 };
