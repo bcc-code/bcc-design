14fc4ea94d639e7f51938e69d681f9fdc80eb5cf
Jan-Sverre Riksfjord <jasverix@gmail.com>
2023-04-22 14:58:38 +0200

    convert the BccPin component to use css classes - removed xxs size since it does not exist in tailwind

--- src/components/BccPin/BccPin.vue
+++ src/components/BccPin/BccPin.vue
@@ -1,12 +1,11 @@
 <script setup lang="ts">
 import type { Component } from "vue";
-import { sizeVariants, variants } from "../../composables/variants";
 
 withDefaults(
   defineProps<{
     icon?: string | Component | Function;
-    size?: keyof typeof sizeVariants;
-    variant?: keyof typeof variants;
+    size?: "xs" | "sm" | "md" | "lg" | "xl" | "2xl";
+    variant?: "neutral" | "danger" | "warning" | "success" | "info" | "systemInfo";
   }>(),
   {
     size: "xs",
@@ -17,9 +16,23 @@ withDefaults(
 
 <template>
   <div
-    class="inline-block rounded-full p-[.3em] shadow-md"
-    :class="[variants[variant], sizeVariants[size] ?? size]"
+    class="bcc-pin"
+    :class="{
+      'bcc-pin-neutral': variant === 'neutral',
+      'bcc-pin-danger': variant === 'danger',
+      'bcc-pin-warning': variant === 'warning',
+      'bcc-pin-success': variant === 'success',
+      'bcc-pin-info': variant === 'info',
+      'bcc-pin-systeminfo': variant === 'systemInfo',
+
+      'bcc-pin-xs': size === 'xs',
+      'bcc-pin-sm': size === 'sm',
+      'bcc-pin-md': size === 'md',
+      'bcc-pin-lg': size === 'lg',
+      'bcc-pin-xl': size === 'xl',
+      'bcc-pin-2xl': size === '2xl',
+    }"
   >
-    <component :is="icon" class="h-[1.2em] w-[1.2em]" />
+    <component :is="icon" class="bcc-pin-icon" />
   </div>
 </template>
