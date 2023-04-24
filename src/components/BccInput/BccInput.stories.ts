5622a2ec05b504672449456e29f5c57743176bcd
timclausen <tim@dife.dk>
2023-04-24 14:39:05 +0200

    Add label for optional input fields (#66)
    + Move input inside label + pass through style attributes to wrapping div
    
    ---------
    
    Co-authored-by: Laurens Groeneveld <laurens.groeneveld@bcc.no>

--- src/components/BccInput/BccInput.stories.ts
+++ src/components/BccInput/BccInput.stories.ts
@@ -40,19 +40,43 @@ Example.parameters = {
 Example.args = {
   state: "default",
   disabled: false,
+  required: true,
   placeholder: "Example placeholder",
   slotDefault: "",
   label: "Example label",
+  optionalLabel: "Optional",
 };
 
 export const State: StoryFn<typeof BccInput> = () => ({
   components: { BccInput },
   template: `
     <div class="inline-flex flex-col space-y-4">
-      <BccInput value="Default" placeholder="Example placeholder" />
-      <BccInput value="Default disabled" placeholder="Example placeholder" :disabled="true" />
-      <BccInput value="Error" placeholder="Example placeholder" state="error" />
-      <BccInput value="Success" placeholder="Example placeholder" state="success" />
+      <BccInput value="Default" placeholder="Example placeholder" required />
+      <BccInput value="Default disabled" placeholder="Example placeholder" :disabled="true" required />
+      <BccInput value="Error" placeholder="Example placeholder" state="error" required />
+      <BccInput value="Success" placeholder="Example placeholder" state="success" required />
+    </div>
+  `,
+});
+
+export const WithLabel: StoryFn<typeof BccInput> = () => ({
+  components: { BccInput },
+  template: `
+    <div class="flex flex-col space-y-4">
+      <BccInput label="Example Label" placeholder="Example placeholder" required />
+      <BccInput label="Example Label that is really long but should still display" placeholder="Example placeholder" required class="w-1/4" />
+    </div>
+  `,
+});
+
+export const Optional: StoryFn<typeof BccInput> = () => ({
+  components: { BccInput },
+  template: `
+    <div class="flex flex-col space-y-4">
+      <BccInput label="Default" placeholder="With label and optional" :required="false" class="w-1/2" />
+      <BccInput placeholder="Without label" :required="false" class="w-1/2" />
+      <BccInput label="Label" optionalLabel="Optional label that is pretty long and should truncate" placeholder="Long optional label" :required="false" class="w-1/4" />
+      <BccInput label="Pretty long label as well" optionalLabel="Optional label that is pretty long and should truncate" placeholder="Long label and optional label" :required="false" class="w-1/4" />
     </div>
   `,
 });
@@ -61,9 +85,9 @@ export const WithMessage: StoryFn<typeof BccInput> = () => ({
   components: { BccInput },
   template: `
     <div class="inline-flex flex-col space-y-4">
-      <BccInput value="Default" placeholder="Example placeholder">This is an example message with <strong>styling</strong>.</BccInput>
-      <BccInput value="Error" placeholder="Example placeholder" state="error">This is an example message with <strong>styling</strong>.</BccInput>
-      <BccInput value="Success" placeholder="Example placeholder" state="success">This is an example message with <strong>styling</strong>.</BccInput>
+      <BccInput value="Default" placeholder="Example placeholder" required>This is an example message with <strong>styling</strong>.</BccInput>
+      <BccInput value="Error" placeholder="Example placeholder" state="error" required>This is an example message with <strong>styling</strong>.</BccInput>
+      <BccInput value="Success" placeholder="Example placeholder" state="success" required>This is an example message with <strong>styling</strong>.</BccInput>
     </div>
   `,
 });
--- src/components/BccInput/BccInput.vue
+++ src/components/BccInput/BccInput.vue
@@ -5,30 +5,58 @@ export default {
 </script>
 
 <script setup lang="ts">
+import { computed } from "vue";
+import { useAttrs } from "vue";
+
 type Props = {
   state?: "default" | "error" | "success";
   disabled?: boolean;
   label?: string;
+  optionalLabel?: string;
+  required?: boolean;
 };
 
 withDefaults(defineProps<Props>(), {
   state: "default",
   disabled: false,
+  required: false,
+  optionalLabel: "Optional",
+});
+
+const attrs = useAttrs();
+const attrsWithoutStyles = computed(() => {
+  let returnObj = {};
+  for (const attr in attrs) {
+    if (attr !== "class" && attr !== "style") {
+      returnObj[attr] = attrs[attr];
+    }
+  }
+  return returnObj;
 });
 </script>
 
 <template>
-  <span class="inline-flex flex-col space-y-2">
-    <label class="bcc-input-label" v-if="label">{{ label }}</label>
-    <input
-      :disabled="disabled"
-      class="bcc-input"
-      :class="{
-        'bcc-input-error': state === 'error',
-        'bcc-input-success': state === 'success',
-      }"
-      v-bind="$attrs"
-    />
+  <div class="inline-flex flex-col space-y-2" :class="$attrs['class']" :style="$attrs['style']">
+    <label class="space-y-2">
+      <span
+        v-if="label || !required"
+        class="flex gap-x-2"
+        :class="{ 'justify-between': label && !required, 'justify-end': !label && !required }"
+      >
+        <span v-if="label" class="bcc-input-label">{{ label }}</span>
+        <span v-if="!required" class="bcc-input-optional-label">{{ optionalLabel }}</span>
+      </span>
+      <input
+        :disabled="disabled"
+        :required="required"
+        class="bcc-input"
+        :class="{
+          'bcc-input-error': state === 'error',
+          'bcc-input-success': state === 'success',
+        }"
+        v-bind="attrsWithoutStyles"
+      />
+    </label>
     <span
       v-if="$slots.default"
       :class="{
@@ -39,5 +67,5 @@ withDefaults(defineProps<Props>(), {
     >
       <slot></slot>
     </span>
-  </span>
+  </div>
 </template>
