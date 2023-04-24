070817d56be9fccd59d8b5a9bc9090c351c08544
Laurens Groeneveld <laurens.groeneveld@bcc.no>
2023-04-24 15:48:20 +0200

    Remove outdated bash scripts for component creation

--- scripts/templates/create-test-file.sh
+++
@@ -1,8 +0,0 @@
-echo "import { describe, it, expect } from \"vitest\";
-
-import { mount } from \"@vue/test-utils\";
-import $1 from \"./$1.vue\";
-
-describe(\"$1\", () => {
-
-});" > src/components/$1/$1.spec.ts
