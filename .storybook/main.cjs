e736f5d8c993e799501e7a8c9abb7c0e645ed868
Laurens Groeneveld <laurens.groeneveld@bcc.no>
2023-04-26 15:47:56 +0200

    Upgrade to Storybook 7.0.7 (#71)

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,13 +1,6 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-  ],
+  "stories": ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", "@storybook/addon-mdx-gfm"],
   "framework": {
     name: "@storybook/vue3-vite",
     options: {}
