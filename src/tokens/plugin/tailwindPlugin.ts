cf46656cefb5a43633f2b8de694d0bd8158a73d4
Laurens Groeneveld <laurens.groeneveld@bcc.no>
2023-06-05 12:28:01 +0200

    WIP

--- src/tokens/plugin/tailwindPlugin.ts
+++ src/tokens/plugin/tailwindPlugin.ts
@@ -1,9 +1,9 @@
 import { bccForbundetTheme } from "../../tokens";
 
 const tailwindPlugin = function ({ addBase }) {
-  const colors = bccForbundetTheme.theme.extend.colors;
+  const colors = bccForbundetTheme.theme!.extend!.colors;
 
-  for (const [colorKey] of Object.entries(colors)) {
+  for (const [colorKey] of Object.entries(colors!)) {
     for (const [colorWeightKey, colorWeightToken] of Object.entries(colors[colorKey])) {
       const variableName = `--${colorKey}-${colorWeightKey}`;
       addBase({
