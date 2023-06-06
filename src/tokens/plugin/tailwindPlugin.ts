a243e838ace2dcd62b99d9af15b1a6d844c0f31b
Laurens Groeneveld <laurens.groeneveld@bcc.no>
2023-06-06 15:42:58 +0200

    Export Tailwind plugin (#83)

---
+++ src/tokens/plugin/tailwindPlugin.ts
@@ -0,0 +1,18 @@
+import { bccForbundetTheme } from "../../tokens";
+
+const tailwindPlugin = function ({ addBase }: any) {
+  const colors = bccForbundetTheme.theme!.extend!.colors;
+
+  for (const [colorKey, colorObject] of Object.entries(colors!)) {
+    for (const colorWeightKey in colorObject) {
+      const variableName = `--${colorKey}-${colorWeightKey}`;
+      addBase({
+        ":root": {
+          [variableName]: colorObject[colorWeightKey], // e.g. --neutral-600: #4b5563
+        },
+      });
+    }
+  }
+};
+
+export default tailwindPlugin;
