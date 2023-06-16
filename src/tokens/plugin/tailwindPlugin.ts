f114b4128a5dd08ce45251799827cf513a9a1c42
Laurens Groeneveld <laurens.groeneveld@bcc.no>
2023-06-16 11:05:44 +0200

    Remove unneeded extra color conversion from plugin

--- src/tokens/plugin/tailwindPlugin.ts
+++ src/tokens/plugin/tailwindPlugin.ts
@@ -1,21 +1,7 @@
-import bccForbundetTheme from "../tailwind/bccForbundetTheme";
 import globalCssVariables from "../variables/global";
 import brandCssVariables from "../variables/brand";
 
 const tailwindPlugin = function ({ addBase }: any) {
-  const colors = bccForbundetTheme.theme!.extend!.colors;
-
-  for (const [colorKey, colorObject] of Object.entries(colors!)) {
-    for (const colorWeightKey in colorObject) {
-      const variableName = `--${colorKey}-${colorWeightKey}`;
-      addBase({
-        ":root": {
-          [variableName]: colorObject[colorWeightKey], // e.g. --neutral-600: #4b5563
-        },
-      });
-    }
-  }
-
   for (const [variableName, variableValue] of Object.entries(globalCssVariables)) {
     addBase({
       ":root": {
