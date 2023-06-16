90a4e32badaedd30d33dcbf06543c9d9800b6b51
Laurens Groeneveld <laurens.groeneveld@bcc.no>
2023-06-16 11:13:38 +0200

    Add brand tokens (#100)
    + Convert reference colors to CSS variables
    + Update Storybook

--- src/tokens/plugin/tailwindPlugin.ts
+++ src/tokens/plugin/tailwindPlugin.ts
@@ -1,23 +1,18 @@
-import bccForbundetTheme from "../tailwind/bccForbundetTheme";
-import { cssVariables } from "../variables/variables";
+import globalCssVariables from "../variables/global";
+import brandCssVariables from "../variables/brand";
 
 const tailwindPlugin = function ({ addBase }: any) {
-  const colors = bccForbundetTheme.theme!.extend!.colors;
-
-  for (const [colorKey, colorObject] of Object.entries(colors!)) {
-    for (const colorWeightKey in colorObject) {
-      const variableName = `--${colorKey}-${colorWeightKey}`;
-      addBase({
-        ":root": {
-          [variableName]: colorObject[colorWeightKey], // e.g. --neutral-600: #4b5563
-        },
-      });
-    }
-  }
-
-  for (const [variableName, variableValue] of Object.entries(cssVariables)) {
+  for (const [variableName, variableValue] of Object.entries(globalCssVariables)) {
     addBase({
       ":root": {
+        [variableName]: variableValue,
+      },
+    });
+  }
+
+  for (const [variableName, variableValue] of Object.entries(brandCssVariables)) {
+    addBase({
+      "[data-context=\"brand\"]": {
         [variableName]: variableValue,
       },
     });
--- src/tokens/scripts/convertToTailwind.cjs
+++ src/tokens/scripts/convertToTailwind.cjs
@@ -3,36 +3,43 @@ const fs = require("fs").promises;
 const semanticColors = ["success", "warning", "danger", "info", "emphasis"];
 let semanticTokens = {};
 
-let tokenCssVariables = {};
+let globalCssVariables = {};
+let brandCssVariables = {};
 
-function getCssVariable(tokenKey, tokenValue) {
-  if (!tokenValue.startsWith("{colors.") && !tokenValue.startsWith("rgb")) {
-    throw Error("Token value not formatted properly");
-  }
-
-  let cssVariableValue = "";
-
-  // It's probably a color function like rgba()
+// Convert a token value into something that can be used as the value of a CSS variable
+function getCssVariableValue(tokenValue) {
+  // It might be a color function like rgba() or a direct hex color value
   if (!tokenValue.startsWith("{colors.")) {
-    cssVariableValue = tokenValue;
-  } else {
-    cssVariableValue = tokenValue.replaceAll("{colors.", "var(--").replaceAll("}", ")").replaceAll(".", "-");
+    return tokenValue;
   }
+  
+  // Convert a token from {colors.neutral.900} to var(--neutral-900)
+  return tokenValue.replaceAll("{colors.", "var(--").replaceAll("}", ")").replaceAll(".", "-");
+}
 
+// Get a CSS variable name from a token key, and save its value to a global object
+function getCssVariable(tokenKey, tokenValue, brandTokenValue = null) {
   let cssVariableKey = tokenKey.replaceAll(".", "-");
 
+  // Remove the -default suffix to simplify the variable name
   if (cssVariableKey.toLowerCase().endsWith("-default")) {
     cssVariableKey = cssVariableKey.slice(0, -8);
   }
 
-  tokenCssVariables[`--${cssVariableKey}`] = cssVariableValue;
+  // Save value of this variable to the global export of CSS variable values
+  globalCssVariables[`--${cssVariableKey}`] = getCssVariableValue(tokenValue);
 
+  // Save value of the corresponding brand token to the global export of brand variables
+  if (brandTokenValue) {
+    brandCssVariables[`--${cssVariableKey}`] = getCssVariableValue(brandTokenValue);
+  }
+
+  // Return the key formatted as a CSS variable
   const cssVariable = `var(--${cssVariableKey})`;
-
   return cssVariable;
 }
 
-function getNestedColors(variants, type, name) {
+function getNestedColors(variants, type, name, brandVariants = null) {
   let colors = {};
 
   // variantKey = primary, secondary etc. or success, danger etc.
@@ -45,7 +52,13 @@ function getNestedColors(variants, type, name) {
         // tokenKey = default, hover, pressed etc.
         // tokenValue = hex color value
         for (let [tokenKey, tokenValue] of Object.entries(itemValue)) {
-          colors[variantKey][tokenKey] = getCssVariable(`${name}-${variantKey}-${tokenKey}`, tokenValue.value);
+          // Get the brand variant for this token if it exists
+          let brandVariant = null;
+          if (brandVariants && brandVariants[variantKey][itemKey][tokenKey]) {
+            brandVariant = brandVariants[variantKey][itemKey][tokenKey].value;
+          }
+
+          colors[variantKey][tokenKey] = getCssVariable(`${name}-${variantKey}-${tokenKey}`, tokenValue.value, brandVariant);
         }
       }
     }
@@ -54,51 +67,58 @@ function getNestedColors(variants, type, name) {
   return colors;
 }
 
-async function writeTailwindConfig(file, content) {
+async function writeTailwindConfig(name, colors) {
+  let content = `export const ${name} = ${JSON.stringify(colors, null, 2)};`;
+
   // Simplifies class names so bg-info-default becomes bg-info
   content = content.replaceAll("default", "DEFAULT");
 
-  await fs.writeFile(file, content, "utf8");
+  await fs.writeFile(`./src/tokens/tailwind/${name}.ts`, content, "utf8");
 }
 
+// Write all the saved CSS variable values to a global file, to be used in the Tailwind plugin
 async function writeCssVariables() {
-  let content = `export const cssVariables = ${JSON.stringify(tokenCssVariables, null, 2)};`;
-  await fs.writeFile("./src/tokens/variables/variables.ts", content, "utf8");
+  let content = `const cssVariables = ${JSON.stringify(globalCssVariables, null, 2)};\n\nexport default cssVariables;`;
+  await fs.writeFile("./src/tokens/variables/global.ts", content, "utf8");
+  
+  let brandContent = `const cssVariables = ${JSON.stringify(brandCssVariables, null, 2)};\n\nexport default cssVariables;`;
+  await fs.writeFile("./src/tokens/variables/brand.ts", brandContent, "utf8");
 }
 
-async function writeColors(figmaInput) {
+// Write the global reference colors, like neutral-900
+async function writeReferenceColors(figmaInput) {
   let colors = figmaInput.reference.colors;
 
   for (let [colorKey] of Object.entries(colors)) {
     for (let [colorWeightKey, colorWeightToken] of Object.entries(colors[colorKey])) {
-      colors[colorKey][colorWeightKey] = colorWeightToken.value;
+      colors[colorKey][colorWeightKey] = getCssVariable(`${colorKey}-${colorWeightKey}`, colorWeightToken.value);
     }
   }
 
-  let content = `export const colors = ${JSON.stringify(colors, null, 2)};`;
-
-  await writeTailwindConfig("./src/tokens/tailwind/colors.ts", content);
+  await writeTailwindConfig("colors", colors);
 }
 
 async function writeTextColors(aliasTokens) {
   // Global text colors
   const globalTextColor = aliasTokens.global.foreground;
+  const brandTextColor = aliasTokens.brand.foreground;
 
   for (let [tokenKey, tokenValue] of Object.entries(globalTextColor)) {
-    globalTextColor[tokenKey] = getCssVariable("text-" + tokenKey, tokenValue.value);
+    globalTextColor[tokenKey] = getCssVariable("text-" + tokenKey, tokenValue.value, brandTextColor[tokenKey].value);
   }
 
   // Semantic text colors
   const semanticForegroundColors = getNestedColors(semanticTokens, "foreground", "text");
   
   const interactiveForegroundColors = aliasTokens.global.interactive;
+  const brandInteractiveForegroundColors = aliasTokens.brand.interactive;
 
   for (let [tokenKey, tokenValue] of Object.entries(interactiveForegroundColors)) {
-    interactiveForegroundColors[tokenKey] = getCssVariable(`text-interactive-${tokenKey}`, tokenValue.value);
+    interactiveForegroundColors[tokenKey] = getCssVariable(`text-interactive-${tokenKey}`, tokenValue.value, brandInteractiveForegroundColors[tokenKey].value);
   }
 
   // Button text colors
-  const buttonForegroundColors = getNestedColors(aliasTokens.global.button, "foreground", "text-button");
+  const buttonForegroundColors = getNestedColors(aliasTokens.global.button, "foreground", "text-button", aliasTokens.brand.button);
   const dangerButtonForegroundColors = getNestedColors(aliasTokens.danger.button, "foreground", "text-button-danger");
 
   const textColor = {
@@ -111,24 +131,23 @@ async function writeTextColors(aliasTokens) {
     },
   };
 
-  let content = `export const textColor = ${JSON.stringify(textColor, null, 2)};`;
-
-  await writeTailwindConfig("./src/tokens/tailwind/textColor.ts", content);
+  await writeTailwindConfig("textColor", textColor);
 }
 
 async function writeBorderColors(aliasTokens) {
   // Global border
   const globalBorderColor = aliasTokens.global.border;
+  const brandBorderColor = aliasTokens.brand.border;
 
   for (let [tokenKey, tokenValue] of Object.entries(globalBorderColor)) {
-    globalBorderColor[tokenKey] = getCssVariable("border-" + tokenKey, tokenValue.value)
+    globalBorderColor[tokenKey] = getCssVariable("border-" + tokenKey, tokenValue.value, brandBorderColor[tokenKey].value);
   }
 
   // Semantic border
   const semanticBorderColors = getNestedColors(semanticTokens, "border", "border");
 
   // Button border
-  const buttonBorderColors = getNestedColors(aliasTokens.global.button, "border", "border-button");
+  const buttonBorderColors = getNestedColors(aliasTokens.global.button, "border", "border-button", aliasTokens.brand.button);
   const dangerButtonBorderColors = getNestedColors(aliasTokens.danger.button, "border", "border-button-danger");
 
   const borderColor = {
@@ -140,23 +159,19 @@ async function writeBorderColors(aliasTokens) {
     },
   };
 
-  let content = `export const borderColor = ${JSON.stringify(borderColor, null, 2)};`;
-  await writeTailwindConfig("./src/tokens/tailwind/borderColor.ts", content);
-
-  let outlineContent = `export const outlineColor = ${JSON.stringify(borderColor, null, 2)};`;
-  await writeTailwindConfig("./src/tokens/tailwind/outlineColor.ts", outlineContent);
-
-  let ringContent = `export const ringColor = ${JSON.stringify(borderColor, null, 2)};`;
-  await writeTailwindConfig("./src/tokens/tailwind/ringColor.ts", ringContent);
+  await writeTailwindConfig("borderColor", borderColor);
+  await writeTailwindConfig("outlineColor", borderColor);
+  await writeTailwindConfig("ringColor", borderColor);
 }
 
 async function writeBackgroundColors(aliasTokens) {
   // Normal background
   const backgroundColors = aliasTokens.global.background;
+  const brandBackgroundColors = aliasTokens.brand.background;
 
   for (let [variantKey] of Object.entries(backgroundColors)) {
     for (let [tokenKey, tokenValue] of Object.entries(backgroundColors[variantKey])) {
-      backgroundColors[variantKey][tokenKey] = getCssVariable(`bg-${variantKey}-${tokenKey}`, tokenValue.value)
+      backgroundColors[variantKey][tokenKey] = getCssVariable(`bg-${variantKey}-${tokenKey}`, tokenValue.value, brandBackgroundColors[variantKey][tokenKey]?.value)
     }
   }
 
@@ -164,7 +179,7 @@ async function writeBackgroundColors(aliasTokens) {
   const semanticBackgroundColors = getNestedColors(semanticTokens, "background", "bg");
 
   // Button background
-  const buttonBackgroundColors = getNestedColors(aliasTokens.global.button, "background", "bg-button");
+  const buttonBackgroundColors = getNestedColors(aliasTokens.global.button, "background", "bg-button", aliasTokens.brand.button);
   const dangerButtonBackgroundColors = getNestedColors(aliasTokens.danger.button, "background", "bg-button-danger");
 
   const backgroundColor = {
@@ -176,9 +191,7 @@ async function writeBackgroundColors(aliasTokens) {
     },
   };
 
-  let content = `export const backgroundColor = ${JSON.stringify(backgroundColor, null, 2)};`;
-
-  await writeTailwindConfig("./src/tokens/tailwind/backgroundColor.ts", content);
+  await writeTailwindConfig("backgroundColor", backgroundColor);
 }
 
 async function getFigmaInput() {
@@ -200,7 +213,7 @@ async function main() {
 
   setSemanticTokens(figmaInput["alias tokens"]);
 
-  await writeColors(figmaInput);
+  await writeReferenceColors(figmaInput);
   await writeTextColors(figmaInput["alias tokens"]);
   await writeBorderColors(figmaInput["alias tokens"]);
   await writeBackgroundColors(figmaInput["alias tokens"]);
