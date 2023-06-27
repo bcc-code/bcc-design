fdf1ccf407b18b97bc4dc35c9e3b9107531c01fb
Laurens Groeneveld <laurens.groeneveld@bcc.no>
2023-06-27 14:42:14 +0200

    Fix icons not always loading in Storybook (#113)

--- src/components/BccButton/BccButton.stories.ts
+++ src/components/BccButton/BccButton.stories.ts
@@ -2,13 +2,6 @@ import BccButton from "./BccButton.vue";
 import { SearchIcon, ChevronRightIcon, ChevronLeftIcon } from "@bcc-code/icons-vue";
 
 import type { Meta, StoryFn } from "@storybook/vue3";
-import { setup } from "@storybook/vue3";
-
-setup((app) => {
-  app.component("ChevronRightIcon", ChevronRightIcon);
-  app.component("ChevronLeftIcon", ChevronLeftIcon);
-  app.component("SearchIcon", SearchIcon);
-});
 
 export default {
   title: "Components/BccButton",
@@ -48,12 +41,12 @@ export default {
 } as Meta<typeof BccButton>;
 
 const Template: StoryFn<typeof BccButton> = (args) => ({
-  components: { BccButton, SearchIcon },
+  components: { BccButton },
   setup() {
-    return { args };
+    return { args, SearchIcon };
   },
   template: `
-    <BccButton v-bind="args" icon="SearchIcon">
+    <BccButton v-bind="args" :icon="SearchIcon">
       {{ args.slotDefault }}
     </BccButton>
   `,
@@ -150,41 +143,44 @@ export const Rounded: StoryFn<typeof BccButton> = () => ({
 });
 
 export const WithIcon: StoryFn<typeof BccButton> = () => ({
-  components: { BccButton, SearchIcon },
+  components: { BccButton },
+  setup() {
+    return { SearchIcon };
+  },
   template: `
     <div class="flex items-start gap-x-2">
-      <BccButton icon="SearchIcon">
+      <BccButton :icon="SearchIcon">
         With left icon
       </BccButton>
-      <BccButton icon="SearchIcon" icon-right>
+      <BccButton :icon="SearchIcon" icon-right>
         With right icon
       </BccButton>
     </div>
     <div class="flex items-start gap-x-2 mt-4">
-      <BccButton variant="secondary" icon="SearchIcon">
+      <BccButton variant="secondary" :icon="SearchIcon">
         Secondary with icon
       </BccButton>
-      <BccButton variant="tertiary" icon="SearchIcon">
+      <BccButton variant="tertiary" :icon="SearchIcon">
         Tertiary with icon
       </BccButton>
-      <BccButton :disabled="true" icon="SearchIcon">
+      <BccButton :disabled="true" :icon="SearchIcon">
         Disabled with icon
       </BccButton>
     </div>
     <div class="flex items-start gap-x-2 mt-4">
-      <BccButton size="xs" icon="SearchIcon">
+      <BccButton size="xs" :icon="SearchIcon">
         xs button
       </BccButton>
-      <BccButton size="sm" icon="SearchIcon">
+      <BccButton size="sm" :icon="SearchIcon">
         sm button
       </BccButton>
-      <BccButton size="base" icon="SearchIcon">
+      <BccButton size="base" :icon="SearchIcon">
         base button
       </BccButton>
-      <BccButton size="lg" icon="SearchIcon">
+      <BccButton size="lg" :icon="SearchIcon">
         lg button
       </BccButton>
-      <BccButton size="xl" icon="SearchIcon">
+      <BccButton size="xl" :icon="SearchIcon">
         xl button
       </BccButton>
     </div>
@@ -192,47 +188,53 @@ export const WithIcon: StoryFn<typeof BccButton> = () => ({
 });
 
 export const IconOnly: StoryFn<typeof BccButton> = () => ({
-  components: { BccButton, SearchIcon },
+  components: { BccButton },
+  setup() {
+    return { SearchIcon };
+  },
   template: `
     <div class="flex items-start gap-x-2">
-      <BccButton icon="SearchIcon" />
-      <BccButton icon-right icon="SearchIcon" />
-      <BccButton rounded icon="SearchIcon" />
-      <BccButton variant="secondary" icon="SearchIcon" />
-      <BccButton variant="tertiary" icon="SearchIcon" />
-      <BccButton disabled icon="SearchIcon" />
+      <BccButton :icon="SearchIcon" />
+      <BccButton icon-right :icon="SearchIcon" />
+      <BccButton rounded :icon="SearchIcon" />
+      <BccButton variant="secondary" :icon="SearchIcon" />
+      <BccButton variant="tertiary" :icon="SearchIcon" />
+      <BccButton disabled :icon="SearchIcon" />
     </div>
     <div class="flex items-start gap-x-2 mt-4">
-      <BccButton size="xs" icon="SearchIcon" />
-      <BccButton size="sm" icon="SearchIcon" />
-      <BccButton size="base" icon="SearchIcon" />
-      <BccButton size="lg" icon="SearchIcon" />
-      <BccButton size="xl" icon="SearchIcon" />
+      <BccButton size="xs" :icon="SearchIcon" />
+      <BccButton size="sm" :icon="SearchIcon" />
+      <BccButton size="base" :icon="SearchIcon" />
+      <BccButton size="lg" :icon="SearchIcon" />
+      <BccButton size="xl" :icon="SearchIcon" />
     </div>
   `,
 });
 
 export const ContentPosition: StoryFn<typeof BccButton> = () => ({
-  components: { BccButton, ChevronRightIcon, ChevronLeftIcon },
+  components: { BccButton },
+  setup() {
+    return { ChevronRightIcon, ChevronLeftIcon };
+  },
   template: `
     <div class="space-y-4">
       <BccButton variant="primary" class="w-full">
         Default text
       </BccButton>
-      <BccButton class="w-full" size="lg" variant="secondary" :rounded="true" :disabled="true" icon="ChevronLeftIcon">
+      <BccButton class="w-full" size="lg" variant="secondary" :rounded="true" :disabled="true" :icon="ChevronLeftIcon">
         Default text, icon left
       </BccButton>
-      <BccButton class="w-full" size="lg" variant="secondary" icon-right icon="ChevronRightIcon">
+      <BccButton class="w-full" size="lg" variant="secondary" icon-right :icon="ChevronRightIcon">
         Default text, icon right
       </BccButton>
       
       <BccButton variant="primary" class="w-full" :center="false">
         Non-centered text
       </BccButton>
-      <BccButton class="w-full" size="lg" variant="secondary" :rounded="true" :disabled="true" :center="false" icon="ChevronLeftIcon">
+      <BccButton class="w-full" size="lg" variant="secondary" :rounded="true" :disabled="true" :center="false" :icon="ChevronLeftIcon">
         Non-centered text, icon left
       </BccButton>
-      <BccButton class="w-full" size="lg" variant="secondary" icon-right :center="false" icon="ChevronRightIcon">
+      <BccButton class="w-full" size="lg" variant="secondary" icon-right :center="false" :icon="ChevronRightIcon">
         Non-centered text, icon right
       </BccButton>
     </div>
