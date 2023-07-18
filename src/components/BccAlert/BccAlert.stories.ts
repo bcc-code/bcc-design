6a0800f26236ff190c6b482947d88e652e9bc1a9
Laurens Groeneveld <laurens.groeneveld@bcc.no>
2023-07-18 17:23:39 +0200

    Add title and icon

--- src/components/BccAlert/BccAlert.stories.ts
+++ src/components/BccAlert/BccAlert.stories.ts
@@ -27,6 +27,53 @@ const Template: StoryFn<typeof BccAlert> = (args) => ({
 
 export const Example = Template.bind({});
 Example.args = {
-  context: "info",
-  slotDefault: "Successfully uploaded.",
+  context: "success",
+  icon: true,
+  slotDefault: "Successfully uploaded",
+  title: "Well done!",
 };
+
+/**
+ * Set the `context` prop to adjust the color of the alert. Pass content to the `default` slot to display text.
+ */
+export const Basic: StoryFn<typeof BccAlert> = () => ({
+  components: { BccAlert },
+  template: `
+    <div class="flex flex-col space-y-4">
+      <BccAlert><strong>Oh snap!</strong> You might want to check this out!</BccAlert>
+      <BccAlert context="success"><strong>Oh snap!</strong> You might want to check this out!</BccAlert>
+      <BccAlert context="warning"><strong>Oh snap!</strong> You might want to check this out!</BccAlert>
+      <BccAlert context="danger"><strong>Oh snap!</strong> You might want to check this out!</BccAlert>
+    </div>
+  `,
+});
+
+/**
+ * Set the `icon` prop to render an icon determined by the `context`
+ */
+export const WithIcon: StoryFn<typeof BccAlert> = () => ({
+  components: { BccAlert },
+  template: `
+    <div class="flex flex-col space-y-4">
+      <BccAlert icon><strong>Oh snap!</strong> You might want to check this out!</BccAlert>
+      <BccAlert icon context="success"><strong>Oh snap!</strong> You might want to check this out!</BccAlert>
+      <BccAlert icon context="warning"><strong>Oh snap!</strong> You might want to check this out!</BccAlert>
+      <BccAlert icon context="danger"><strong>Oh snap!</strong> You might want to check this out!</BccAlert>
+    </div>
+  `,
+});
+
+/**
+ * Set the `title` prop to render a title above the context, optionally next to the icon
+ */
+export const WithTitle: StoryFn<typeof BccAlert> = () => ({
+  components: { BccAlert },
+  template: `
+    <div class="flex flex-col space-y-4">
+      <BccAlert title="Well done!" icon>You might want to check this out!</BccAlert>
+      <BccAlert title="Well done!" icon context="success">You might want to check this out!</BccAlert>
+      <BccAlert title="Well done!" icon context="warning">You might want to check this out!</BccAlert>
+      <BccAlert title="Well done!" icon context="danger">You might want to check this out!</BccAlert>
+    </div>
+  `,
+});
--- src/components/BccAlert/BccAlert.vue
+++ src/components/BccAlert/BccAlert.vue
@@ -1,10 +1,15 @@
 <script setup lang="ts">
+import { TaskAltIcon, InfoIcon, WarningIcon, ErrorIcon } from "@bcc-code/icons-vue";
+
 type Props = {
   context?: "info" | "success" | "warning" | "danger";
+  icon?: boolean;
+  title?: string;
 };
 
 withDefaults(defineProps<Props>(), {
   context: "info",
+  icon: false,
 });
 </script>
 
@@ -17,6 +22,15 @@ withDefaults(defineProps<Props>(), {
       'bcc-alert-danger': context == 'danger',
     }"
   >
-    <slot />
+    <span v-if="icon">
+      <InfoIcon class="bcc-alert-icon" v-if="context == 'info'" />
+      <TaskAltIcon class="bcc-alert-icon" v-if="context == 'success'" />
+      <WarningIcon class="bcc-alert-icon" v-if="context == 'warning'" />
+      <ErrorIcon class="bcc-alert-icon" v-if="context == 'danger'" />
+    </span>
+    <h3 v-if="title" class="bcc-alert-title">{{ title }}</h3>
+    <p class="bcc-alert-content">
+      <slot />
+    </p>
   </div>
 </template>
