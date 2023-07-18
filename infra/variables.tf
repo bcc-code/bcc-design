2814ab613776741f1e5fc5d38ab3503e64e1fc84
Adelin Niculae <43118261+adelinn@users.noreply.github.com>
2023-07-18 15:13:24 +0200

    add variables.tf

---
+++ infra/variables.tf
@@ -0,0 +1,9 @@
+variable "props" {
+  type        = string
+  description = "A JSON string containing the properties of the current project."
+}
+
+variable "deployment_props" {
+  type        = string
+  description = "A JSON string containing the properties of the current deployment."
+}
