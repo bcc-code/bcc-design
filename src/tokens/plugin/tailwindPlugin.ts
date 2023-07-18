fd7bd5a03b9f44003e940bb562c16455bd780ae8
Laurens Groeneveld <laurens.groeneveld@bcc.no>
2023-07-18 15:18:45 +0200

    Import Tailwind Forms plugin in our plugin (#141)
    So consumers of the library don't need to install it as a dependency

--- src/tokens/plugin/tailwindPlugin.ts
+++ src/tokens/plugin/tailwindPlugin.ts
@@ -1,7 +1,8 @@
 import globalCssVariables from "../variables/global";
 import alternativeCssVariables from "../variables/alternative";
+import formsPlugin from "@tailwindcss/forms";
 
-const tailwindPlugin = function ({ addBase }: any) {
+const tailwindPlugin = function ({ addBase, addComponents, theme }: any) {
   for (const [variableName, variableValue] of Object.entries(globalCssVariables)) {
     addBase({
       ":root": {
@@ -17,6 +18,12 @@ const tailwindPlugin = function ({ addBase }: any) {
       },
     });
   }
+  
+  formsPlugin({
+    strategy: 'class',
+  })
+  // @ts-ignore Type expects 0 arguments but these need to be passed along to the plugin
+  .handler({addBase, addComponents, theme});
 };
 
 export default tailwindPlugin;
