2a804f1c80d9396c95ea518ac8120da6c45a775d
Laurens Groeneveld <laurens.groeneveld@bcc.no>
2023-07-18 14:05:59 +0200

    Open introduction page by default

--- .storybook/preview.cjs
+++ .storybook/preview.cjs
@@ -15,7 +15,7 @@ const preview = {
   parameters: {
     options: {
       storySort: {
-        order: ['Forms', 'Components', '*'],
+        order: ['Introduction', 'Forms', 'Components', '*'],
       },
     },
   },
