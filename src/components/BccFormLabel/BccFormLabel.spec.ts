a146a37f47d5396e6c1e14b59164d5d9b8cb384f
Laurens Groeneveld <laurens.groeneveld@bcc.no>
2023-07-20 17:13:37 +0200

    Tests

--- src/components/BccFormLabel/BccFormLabel.spec.ts
+++ src/components/BccFormLabel/BccFormLabel.spec.ts
@@ -4,5 +4,21 @@ import { mount } from "@vue/test-utils";
 import BccFormLabel from "./BccFormLabel.vue";
 
 describe("BccFormLabel", () => {
+  it("renders a label", () => {
+    const wrapper = mount(BccFormLabel, {
+      slots: { default: "Test label" },
+    });
 
+    expect(wrapper.text()).toBe("Test label");
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+
+  it("can render an optional label", () => {
+    const wrapper = mount(BccFormLabel, {
+      props: { showOptionalLabel: true, optionalLabel: "Custom optional label" },
+    });
+
+    expect(wrapper.text()).toBe("Custom optional label");
+    expect(wrapper.html()).toMatchSnapshot();
+  });
 });
