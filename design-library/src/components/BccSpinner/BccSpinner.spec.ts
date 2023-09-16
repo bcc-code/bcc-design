994d5ef9a05d4960d420cd9135b7f06b83ac4e4a
Lucas Powell <Lucas-Powell@users.noreply.github.com>
2023-09-16 14:21:01 +0200

    self test

--- design-library/src/components/BccSpinner/BccSpinner.spec.ts
+++ design-library/src/components/BccSpinner/BccSpinner.spec.ts
@@ -3,4 +3,14 @@ import { describe, it, expect } from "vitest";
 import { mount } from "@vue/test-utils";
 import BccSpinner from "./BccSpinner.vue";
 
-describe("BccSpinner", () => {});
+describe("BccSpinner", () => {
+  it("Matches the snaphot", () => {
+    const wrapper = mount(BccSpinner, {});
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+
+  it("contains SVG element", () => {
+    const wrapper = mount(BccSpinner);
+    expect(wrapper.html()).toContain("<svg");
+  });
+});
