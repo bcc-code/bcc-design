f3b95a8d859b5fd5f87465286f9eab8ae0dfacf9
Kristoffer Bekkevold <kristoffer.bekkevold@outlook.com>
2024-03-20 16:34:50 +0100

    Update

--- design-library/src/components/BccPagination/BccPagination.stories.ts
+++ design-library/src/components/BccPagination/BccPagination.stories.ts
@@ -19,3 +19,45 @@ const Template: StoryFn<typeof BccPagination> = (args) => ({
 });
 
 export const Example = Template.bind({});
+Example.args = {
+  totalRecords: 5,
+  perPage: 2,
+  rows: [
+    {
+      id: 1,
+      year_group: "23/24",
+      name: "Ola Nordmann",
+      status: { text: "On Track", context: "success" },
+      progress: {
+        amount: 2,
+      },
+    },
+    {
+      id: 2,
+      year_group: "22/23",
+      name: "Johan Oscar",
+      status: { text: "Finished", context: "info" },
+      progress: {
+        amount: 25,
+      },
+    },
+    {
+      id: 3,
+      year_group: "22/23",
+      name: "Ada Lovelace",
+      status: { text: "Behind Schedule", context: "warning" },
+      progress: {
+        amount: 15,
+      },
+    },
+    {
+      id: 4,
+      year_group: "23/24",
+      name: "Firmus Piett",
+      status: { text: "On Track", context: "success" },
+      progress: {
+        amount: 15,
+      },
+    },
+  ],
+};
--- design-library/src/components/BccPagination/BccPagination.vue
+++ design-library/src/components/BccPagination/BccPagination.vue
@@ -1,29 +1,59 @@
 <script setup lang="ts">
 import { ArrowBackIosIcon } from "@bcc-code/icons-vue";
 import { ArrowForwardIosIcon } from "@bcc-code/icons-vue";
-import { toRefs, type Component } from "vue";
+import { toRefs, ref, computed, watchEffect } from "vue";
+import type { Item } from "../BccTable/BccTable.vue";
 
-export type BccPages = Array<{
-  numbers: number;
-  isSelected: boolean;
-  as?: Component | Function;
-}>;
 type Props = {
-  Pages: BccPages[];
+  rows: Item[];
+  totalRecords: number;
+  perPage: number;
 };
 
 const props = withDefaults(defineProps<Props>(), {});
+const { rows, totalRecords, perPage } = toRefs(props);
 
-const { Pages } = toRefs(props);
+const emit = defineEmits(["update:modelValue"]);
+
+const currentPage = ref(1);
+const totalPages = computed(() => Math.ceil(totalRecords.value / perPage.value));
+
+const goToPage = (numPage: number) => {
+  currentPage.value = numPage;
+};
+
+const isFirstPage = computed(() => currentPage.value === 1);
+const isLastPage = computed(() => currentPage.value === totalPages.value);
+
+const safeRows = computed(() => (Array.isArray(rows.value) ? rows.value : []));
+
+const paginatedRows = computed(() => {
+  const start = (currentPage.value - 1) * perPage.value;
+  const end = start + perPage.value;
+  return safeRows.value.slice(start, end);
+});
+
+watchEffect(() => {
+  emit("update:modelValue", paginatedRows.value);
+});
 </script>
 
 <template>
   <div class="bcc-pagination">
-    <button class="bcc-pagination-button-left">
+    <button :disabled="isFirstPage" class="bcc-pagination-button-left">
       <ArrowBackIosIcon class="bcc-pagination-button-icon" />
     </button>
 
-    <button class="bcc-pagination-button-right">
+    <button
+      v-for="page in totalPages"
+      v-bind:key="page"
+      @click="() => goToPage(page)"
+      class="bcc-pagination-button-page"
+    >
+      <p>{{ page }}</p>
+    </button>
+
+    <button :disabled="isLastPage" class="bcc-pagination-button-right">
       <ArrowForwardIosIcon class="bcc-pagination-button-icon" />
     </button>
   </div>
