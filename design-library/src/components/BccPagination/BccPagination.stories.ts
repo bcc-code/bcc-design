0d812dee1cd5cfdef30a147bf3d670bdc7e4a786
Kristoffer Bekkevold <kristoffer.bekkevold@outlook.com>
2024-03-21 16:02:41 +0100

    Pagination slices rows

--- design-library/src/components/BccPagination/BccPagination.stories.ts
+++ design-library/src/components/BccPagination/BccPagination.stories.ts
@@ -1,28 +1,67 @@
 import BccPagination from "./BccPagination.vue";
+import BccTable from "../BccTable/BccTable.vue";
+import BccButton from "../BccButton/BccButton.vue";
+import BccBadge from "../BccBadge/BccBadge.vue";
+import { ChevronRightIcon } from "@bcc-code/icons-vue";
 
 import type { Meta, StoryFn } from "@storybook/vue3";
+import { ref } from "vue";
 
 export default {
   title: "Other/BccPagination",
   component: BccPagination,
-  argTypes: {},
+  argTypes: {
+    perPage: { control: "number", defaultValue: 2 },
+  },
 } as Meta<typeof BccPagination>;
 
 const Template: StoryFn<typeof BccPagination> = (args) => ({
-  components: { BccPagination },
+  components: { BccPagination, BccTable, BccButton, BccBadge },
   setup() {
-    return { args };
+    const paginatedRows = ref(args.tableItems);
+
+    const columns = ref([
+      {
+        text: "Year group",
+        key: "year_group",
+      },
+      {
+        text: "Name",
+        key: "name",
+      },
+      {
+        text: "Status",
+        key: "status",
+      },
+      {
+        text: "Progress",
+        key: "progress.amount",
+      },
+      {
+        text: "Actions",
+        key: "actions",
+        sortable: false,
+      },
+    ]);
+
+    return { args, paginatedRows, columns, ChevronRightIcon };
   },
   template: `
-    <BccPagination v-bind="args" />
+    <BccTable :items="paginatedRows" :columns="columns"> 
+    <template #item.status="{ item }">
+        <BccBadge :context="item.status.context">{{ item.status.text }}</BccBadge>
+      </template>
+      <template #item.actions="{ item }">
+        <BccButton variant="tertiary" size="sm" :padding="false" :icon="ChevronRightIcon" iconRight>Evaluation</BccButton>
+      </template>
+    </BccTable>
+    <BccPagination :tableItems="args.tableItems" v-model:paginatedRows="paginatedRows" :perPage="args.perPage" />
   `,
 });
 
 export const Example = Template.bind({});
 Example.args = {
-  totalRecords: 5,
-  perPage: 2,
-  rows: [
+  tableItems: [
     {
       id: 1,
       year_group: "23/24",
@@ -57,6 +96,33 @@ Example.args = {
       status: { text: "On Track", context: "success" },
       progress: {
         amount: 15,
+      },
+    },
+    {
+      id: 5,
+      year_group: "22/23",
+      name: "John Pork",
+      status: { text: "On Track", context: "warning" },
+      progress: {
+        amount: 45,
+      },
+    },
+    {
+      id: 6,
+      year_group: "23/24",
+      name: "John Doe",
+      status: { text: "Behind Schedule", context: "info" },
+      progress: {
+        amount: 80,
+      },
+    },
+    {
+      id: 7,
+      year_group: "22/23",
+      name: "Skibidi Toilet",
+      status: { text: "Finished", context: "sucess" },
+      progress: {
+        amount: 100,
       },
     },
   ],
--- design-library/src/components/BccPagination/BccPagination.vue
+++ design-library/src/components/BccPagination/BccPagination.vue
@@ -3,58 +3,101 @@ import { ArrowBackIosIcon } from "@bcc-code/icons-vue";
 import { ArrowForwardIosIcon } from "@bcc-code/icons-vue";
 import { toRefs, ref, computed, watchEffect } from "vue";
 import type { Item } from "../BccTable/BccTable.vue";
+import BccSelect from "../BccSelect/BccSelect.vue";
 
 type Props = {
-  rows: Item[];
-  totalRecords: number;
-  perPage: number;
+  tableItems: Item[];
+  perPage?: number;
+  maxButtonsDisplayed: number;
 };
 
-const props = withDefaults(defineProps<Props>(), {});
-const { rows, totalRecords, perPage } = toRefs(props);
+const props = withDefaults(defineProps<Props>(), {
+  perPage: 2,
+  maxButtonsDisplayed: 2,
+});
+const { tableItems, perPage, maxButtonsDisplayed } = toRefs(props);
 
-const emit = defineEmits(["update:modelValue"]);
+const emit = defineEmits(["update:paginatedRows"]);
 
 const currentPage = ref(1);
-const totalPages = computed(() => Math.ceil(totalRecords.value / perPage.value));
+const totalPages = computed(() => Math.ceil(tableItems.value.length / perPage.value));
+
+const isFirstPage = computed(() => currentPage.value === 1);
+const isLastPage = computed(() => currentPage.value === totalPages.value);
+
+const paginatedRows = computed(() => {
+  const start = (currentPage.value - 1) * perPage.value;
+  const end = start + perPage.value;
+  return tableItems.value.slice(start, end);
+});
+
+const startPage = computed(() => {
+  if (currentPage.value <= maxButtonsDisplayed.value) {
+    return 1;
+  } else if (currentPage.value + maxButtonsDisplayed.value > totalPages.value) {
+    return totalPages.value - maxButtonsDisplayed.value;
+  } else {
+    return currentPage.value - Math.floor(maxButtonsDisplayed.value / 2);
+  }
+});
+
+const endPage = computed(() => {
+  return startPage.value + maxButtonsDisplayed.value;
+});
+
+const currentDisplayedPages = computed(() => {
+  let arr: number[] = [];
+  for (let i: number = startPage.value; i <= endPage.value; i++) {
+    arr.push(i);
+  }
+  return arr;
+});
 
 const goToPage = (numPage: number) => {
   currentPage.value = numPage;
 };
 
-const isFirstPage = computed(() => currentPage.value === 1);
-const isLastPage = computed(() => currentPage.value === totalPages.value);
+const nextPage = () => {
+  currentPage.value += 1;
+};
 
-const safeRows = computed(() => (Array.isArray(rows.value) ? rows.value : []));
-
-const paginatedRows = computed(() => {
-  const start = (currentPage.value - 1) * perPage.value;
-  const end = start + perPage.value;
-  return safeRows.value.slice(start, end);
-});
+const previous = () => {
+  currentPage.value -= 1;
+};
 
 watchEffect(() => {
-  emit("update:modelValue", paginatedRows.value);
+  emit("update:paginatedRows", paginatedRows.value);
 });
 </script>
 
 <template>
   <div class="bcc-pagination">
-    <button :disabled="isFirstPage" class="bcc-pagination-button-left">
-      <ArrowBackIosIcon class="bcc-pagination-button-icon" />
-    </button>
+    <BccSelect :v-model="perPage">
+      <option disabled value="">Select an option...</option>
+      <option :value="1">1</option>
+      <option :value="2">2</option>
+    </BccSelect>
 
-    <button
-      v-for="page in totalPages"
-      v-bind:key="page"
-      @click="() => goToPage(page)"
-      class="bcc-pagination-button-page"
-    >
-      <p>{{ page }}</p>
-    </button>
+    <div class="bcc-pagination-button-container">
+      <button @click="previous" :disabled="isFirstPage" class="bcc-pagination-button-left">
+        <ArrowBackIosIcon class="bcc-pagination-button-icon" />
+      </button>
 
-    <button :disabled="isLastPage" class="bcc-pagination-button-right">
-      <ArrowForwardIosIcon class="bcc-pagination-button-icon" />
-    </button>
+      <button
+        v-for="page in currentDisplayedPages"
+        v-bind:key="page"
+        @click="goToPage(page)"
+        :class="{
+          'bcc-pagination-button-selected': page === currentPage,
+          'bcc-pagination-button-unselected': page !== currentPage,
+        }"
+      >
+        {{ page }}
+      </button>
+
+      <button @click="nextPage" :disabled="isLastPage" class="bcc-pagination-button-right">
+        <ArrowForwardIosIcon class="bcc-pagination-button-icon" />
+      </button>
+    </div>
   </div>
 </template>
