5a2c8644a423e63a16ddba360f5bcc8e0dfbd2a6
Lukasz <lukaspoloki@gmail.com>
2024-04-04 13:48:31 +0200

    Add BccAccordion component (#297)
    * Add BccAccordion component
    
    * oopsie - shouldn't import defineProps
    
    * import from different source?
    
    * this import was unnecessary
    
    * change component's title wrap class
    
    * Add desktop variant to BccAccordion component
    
    * Refactor BccAccordion CSS class names and update component references

---
+++ design-library/src/components/BccAccordion/BccAccordion.spec.ts
@@ -0,0 +1,46 @@
+import { describe, it, expect } from "vitest";
+import { mount } from "@vue/test-utils";
+import BccAccordion from "./BccAccordion.vue";
+
+describe("BccAccordion", () => {
+  it("renders without crashing", () => {
+    const wrapper = mount(BccAccordion, {
+      props: {
+        title: "Test Title",
+      },
+    });
+    expect(wrapper.text()).toContain("Test Title");
+  });
+
+  it("uses props and slots as expected", () => {
+    const wrapper = mount(BccAccordion, {
+      props: {
+        title: "Props Title",
+        subtitle: "Props Subtitle",
+        variant: "readonly",
+        size: "base",
+      },
+      slots: {
+        left: '<div class="test-slot-content">Slot Content</div>',
+      },
+    });
+
+    expect(wrapper.find(".bcc-accordion-title").text()).toBe("Props Title");
+    expect(wrapper.find(".bcc-accordion-subtitle").text()).toBe("Props Subtitle");
+    expect(wrapper.find(".test-slot-content").exists()).toBe(true);
+  });
+
+  it("toggles content visibility on header click", async () => {
+    const wrapper = mount(BccAccordion, {
+      props: {
+        title: "Clickable Title",
+      },
+    });
+
+    const isContentPresentInitially = wrapper.find(".bcc-accordion-content").exists();
+    expect(isContentPresentInitially).toBe(false);
+    await wrapper.find(".bcc-accordion-header").trigger("click");
+    const isContentVisibleAfterClick = wrapper.find(".bcc-accordion-content").exists();
+    expect(isContentVisibleAfterClick).toBe(true);
+  });
+});
---
+++ design-library/src/components/BccAccordion/BccAccordion.vue
@@ -0,0 +1,79 @@
+<script setup lang="ts">
+import { ExpandMoreIcon } from "@bcc-code/icons-vue";
+import { BccBadge, BccPin } from "@/index";
+import { type Component, ref } from "vue";
+
+const variants = {
+  readonly: "bcc-accordion-readonly",
+  brand: "bcc-accordion-brand",
+  interactive: "bcc-accordion-interactive",
+  desktop: "bcc-accordion-desktop",
+};
+
+const sizes = {
+  base: "bcc-accordion-base",
+  lg: "bcc-accordion-lg",
+};
+
+type Props = {
+  title: string;
+  subtitle?: string;
+  variant?: keyof typeof variants;
+  size?: keyof typeof sizes;
+  icon?: string | Component | Function;
+  badge?: InstanceType<typeof BccBadge>["$props"] & { text: string };
+  pin?: InstanceType<typeof BccPin>["$props"];
+};
+
+withDefaults(defineProps<Props>(), {
+  title: "",
+  subtitle: "",
+  variant: "readonly",
+  size: "base",
+  icon: "",
+  badge: undefined,
+  pin: undefined,
+});
+
+const isOpen = ref(false);
+</script>
+
+<template>
+  <div class="bcc-accordion">
+    <div
+      class="bcc-accordion-header"
+      :class="{
+        'bcc-accordion-active': isOpen,
+        [variants[variant]]: true,
+        [sizes[size]]: true,
+      }"
+      @click="() => (isOpen = !isOpen)"
+    >
+      <div class="bcc-accordion-title-wrap">
+        <div>
+          <slot name="left" />
+        </div>
+        <div>
+          <span class="bcc-accordion-title">
+            {{ title }}
+          </span>
+          <span v-if="subtitle" class="bcc-accordion-subtitle">{{ subtitle }}</span>
+        </div>
+      </div>
+      <div class="bcc-accordion-right">
+        <slot name="action" />
+        <component class="bcc-accordion-right-icon bcc-accordion-icon" v-if="icon" :is="icon" />
+        <BccBadge class="bcc-accordion-right-icon" v-if="badge" v-bind="badge">{{
+          badge.text
+        }}</BccBadge>
+        <BccPin class="bcc-accordion-right-icon" v-show="pin" v-bind="pin" />
+        <div class="bcc-accordion-right-icon bcc-accordion-icon">
+          <ExpandMoreIcon />
+        </div>
+      </div>
+    </div>
+    <div v-if="isOpen" class="bcc-accordion-content">
+      <slot />
+    </div>
+  </div>
+</template>
