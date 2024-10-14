bf3bda1fae1ddbb7e3f2ac603081b92c1939c6b1
Gerard Lamusse <gerardlamo@gmail.com>
2024-10-14 12:30:54 +0200

    Update Pin with size bordered and squared options (#322)

--- design-library/src/components/BccPin/BccPin.stories.ts
+++ design-library/src/components/BccPin/BccPin.stories.ts
@@ -1,7 +1,18 @@
 import { CheckCircleIcon } from "@bcc-code/icons-vue";
 import BccPin from "./BccPin.vue";
 
-import type { Meta, StoryFn } from "@storybook/vue3";
+import type { Meta, setup, StoryFn } from "@storybook/vue3";
+
+const ContextOptions = [
+  "neutral",
+  "danger",
+  "warning",
+  "success",
+  "info",
+  "muddy-waters",
+  "mongoose",
+  "brand",
+];
 
 /**
  * A companion to the badge component, to render just an icon or a number like "42" or a counter like "4/10". See the `BccBadge` component for a larger, differently styled version to be used with text.
@@ -12,22 +23,25 @@ export default {
   argTypes: {
     context: {
       description: "Which context color to use for the styling of the pin",
-      options: [
-        "neutral",
-        "danger",
-        "warning",
-        "success",
-        "info",
-        "muddy-waters",
-        "mongoose",
-        "brand",
-      ],
+      options: ContextOptions,
+      control: { type: "radio" },
+    },
+    size: {
+      description: "Size of the pin",
+      options: ["sm", "base", "lg"],
+      default: "base",
       control: { type: "radio" },
     },
     contrast: {
       description: "contrast",
       options: ["light", "dark"],
       control: { type: "radio" },
+    },
+    bordered: {
+      control: { type: "boolean", default: false },
+    },
+    squared: {
+      control: { type: "boolean", default: false },
     },
   },
 } as Meta<typeof BccPin>;
@@ -71,11 +85,7 @@ export const WithIcon: StoryFn<typeof BccPin> = () => ({
   },
   template: `
     <div class="flex items-start space-x-2">
-      <BccPin context="neutral" :icon="CheckCircleIcon" />
-      <BccPin context="danger" :icon="CheckCircleIcon" />
-      <BccPin context="warning" :icon="CheckCircleIcon" />
-      <BccPin context="success" :icon="CheckCircleIcon" />
-      <BccPin context="info" :icon="CheckCircleIcon" />
+      ${ContextOptions.map((o) => `<BccPin context="${o}" :icon="CheckCircleIcon" />`).join("\n")}
     </div>
   `,
 });
@@ -83,25 +93,21 @@ export const WithIcon: StoryFn<typeof BccPin> = () => ({
 /**
  * Set the `contrast` prop to render a pin with a light or dark contrast
  */
-export const Contrast: StoryFn<typeof BccPin> = () => ({
+export const Contrast: StoryFn<typeof BccPin> = (args) => ({
   components: { BccPin },
   setup() {
-    return { CheckCircleIcon };
+    return { CheckCircleIcon, args };
   },
   template: `
     <div class="flex items-start space-x-2 mb-2">
-      <BccPin context="neutral" :icon="CheckCircleIcon" contrast="light" />
-      <BccPin context="danger" :icon="CheckCircleIcon" contrast="light" />
-      <BccPin context="warning" :icon="CheckCircleIcon" contrast="light" />
-      <BccPin context="success" :icon="CheckCircleIcon" contrast="light" />
-      <BccPin context="info" :icon="CheckCircleIcon" contrast="light" />
+      ${ContextOptions.map(
+        (o) => `<BccPin v-bind="args" context="${o}" :icon="CheckCircleIcon" contrast="light" />`
+      ).join("\n")}
     </div>
     <div class="flex items-start space-x-2">
-      <BccPin context="neutral" :icon="CheckCircleIcon" contrast="dark" />
-      <BccPin context="danger" :icon="CheckCircleIcon" contrast="dark" />
-      <BccPin context="warning" :icon="CheckCircleIcon" contrast="dark" />
-      <BccPin context="success" :icon="CheckCircleIcon" contrast="dark" />
-      <BccPin context="info" :icon="CheckCircleIcon" contrast="dark" />
+      ${ContextOptions.map(
+        (o) => `<BccPin v-bind="args" context="${o}" :icon="CheckCircleIcon" contrast="dark" />`
+      ).join("\n")}
     </div>
   `,
 });
@@ -109,15 +115,18 @@ export const Contrast: StoryFn<typeof BccPin> = () => ({
 /**
  * Set the `text` prop to render a text in the pin. If both `icon` and `text` are given only the `icon` is rendered
  */
-export const WithText: StoryFn<typeof BccPin> = () => ({
+export const WithText: StoryFn<typeof BccPin> = (args) => ({
   components: { BccPin },
+  setup() {
+    return { args };
+  },
   template: `
     <div class="flex items-start space-x-2">
-      <BccPin context="neutral" text="1" />
-      <BccPin context="danger" text="2" />
-      <BccPin context="warning" text="10" />
-      <BccPin context="success" text="43" />
-      <BccPin context="info" text="4 / 10" />
+      <BccPin v-bind="args" context="neutral" text="1" />
+      <BccPin v-bind="args" context="danger" text="2" />
+      <BccPin v-bind="args" context="warning" text="10" />
+      <BccPin v-bind="args" context="success" text="43" />
+      <BccPin v-bind="args" context="info" text="4 / 10" />
     </div>
   `,
 });
