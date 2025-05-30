eb5da85c5a8473522bb55e868799d9a9dbc5d7ae
Gerard <gerard.lamusse@bcc.no>
2025-05-30 16:07:57 +0200

    Export type BCC_CONTEXTS

--- design-library/src/types.ts
+++ design-library/src/types.ts
@@ -2,6 +2,8 @@ import type { Component, FunctionalComponent, RenderFunction, VNode } from "vue"
 
 export type VueComponent = string | Component | FunctionalComponent | VNode | RenderFunction;
 
+export { BCC_CONTEXTS } from "@/composables/contexts";
+
 // Additional Types
 export * from "./components/BccTabs/types";
 export * from "./components/BccReact/types";
