347b6cbc23208080d7cbce9660e17b55b36d6858
Jan-Sverre Riksfjord <jan.sverre.riksfjord@bcc.no>
2026-03-05 11:30:28 +0100

    story for dialog

--- component-library/src/stories/primevue/Dialog.stories.ts
+++ component-library/src/stories/primevue/Dialog.stories.ts
@@ -15,6 +15,7 @@ const meta = {
 	argTypes: {
 		modal: { control: 'boolean' },
 		closable: { control: 'boolean' },
+		maximizable: { control: 'boolean' },
 		header: { control: 'text' },
 	},
 } as Meta;
@@ -27,21 +28,22 @@ export const Default: Story = {
 	args: {
 		modal: false,
 		closable: true,
+		maximizable: false,
 		header: 'Dialog Title',
 	},
 	render: args => ({
 		components: { BccDialog, BccButton },
 		setup() {
-			const active = ref(true);
-			return { args, active };
+			const visible = ref(false);
+			return { args, visible };
 		},
 		template: `
 			<div>
-				<BccButton label="Show" @click="active = true" />
-				<BccDialog v-model:visible="active" v-bind="args" :style="{ width: '30rem' }">
+				<BccButton label="Show" @click="visible = true" />
+				<BccDialog v-model:visible="visible" v-bind="args">
 					<p>Dialog content. Close with the header icon or the button below.</p>
 					<template #footer>
-						<BccButton label="Close" severity="secondary" @click="active = false" />
+						<BccButton label="Close" severity="secondary" @click="visible = false" />
 					</template>
 				</BccDialog>
 			</div>
@@ -56,16 +58,18 @@ export const Modal: Story = {
 	render: () => ({
 		components: { BccDialog, BccButton },
 		setup() {
-			const active = ref(false);
-			return { active };
+			const visible = ref(false);
+			return { visible };
 		},
 		template: `
 			<div>
-				<BccButton label="Open modal" @click="active = true" />
-				<BccDialog v-model:visible="active" modal header="Modal Dialog" :style="{ width: '30rem' }">
+				<BccButton label="Open modal" @click="visible = true" />
+				<BccDialog v-model:visible="visible" modal header="Modal Dialog" :style="{ width: '30rem' }">
 					<p>This dialog blocks interaction with the rest of the page.</p>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ullamcorper lorem in egestas luctus. Vestibulum vel lacus vel nunc tincidunt molestie. Vivamus eget velit eu ipsum pellentesque efficitur vel accumsan purus. Mauris auctor velit eget auctor efficitur. Mauris ligula tortor, sodales sit amet arcu ut, sollicitudin rhoncus lorem. Phasellus dapibus sapien augue, at feugiat ligula aliquet eu. Integer eget nulla eu massa blandit finibus at et dui.</p>
 					<template #footer>
-						<BccButton label="Close" severity="secondary" @click="active = false" />
+						<BccButton label="Cancel" severity="secondary" @click="visible = false" />
+						<BccButton label="Save" />
 					</template>
 				</BccDialog>
 			</div>
