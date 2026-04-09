f421703621079b1eee53809e96bac3bc6989dc08
Jan-Sverre Riksfjord <jan.sverre.riksfjord@bcc.no>
2026-04-09 10:23:40 +0200

    replace info icon on message

--- component-library/src/components/wrapped/BccMessage.vue
+++ component-library/src/components/wrapped/BccMessage.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import type { VueComponent } from '@/types';
-import { CheckIcon, ContrastIcon, ErrorIcon, InfoIcon, WarningIcon } from '@bcc-code/icons-vue';
+import { CheckIcon, ContrastIcon, ErrorIcon, InfoFillIcon, InfoIcon, WarningIcon } from '@bcc-code/icons-vue';
 import BccMessage, { type MessageProps as PrimeMessageProps } from 'primevue/message';
 import { computed, type Component } from 'vue';
 
@@ -22,7 +22,7 @@ const messageBindings = computed((): PrimeMessageProps => {
 });
 
 const SeverityIcons: Record<NonNullable<PrimeMessageProps['severity']>, Component> = {
-	info: InfoIcon,
+	info: InfoFillIcon,
 	success: CheckIcon,
 	warn: WarningIcon,
 	error: ErrorIcon,
