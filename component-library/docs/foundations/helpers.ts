40e8eccd05f06e8767b819937966b3881056bfb3
Bogdan Niculae <19570442+bogdaniculae@users.noreply.github.com>
2026-04-09 16:48:48 +0300

    config: remove custom code block styles, fix Do/Don't card height

--- component-library/docs/foundations/helpers.ts
+++ component-library/docs/foundations/helpers.ts
@@ -8,7 +8,7 @@ export function doDont(
 			<div class="flex-1 rounded-lg overflow-hidden flex flex-col">
 				<div class="bg-neutral-100 p-spacing-300 flex items-center justify-center flex-1">${doExample}</div>
 				<div class="border-t-[3px] border-success"></div>
-				<div class="bg-success-default p-spacing-200">
+				<div class="bg-success-default p-spacing-200 flex-1">
 					<div class="flex items-center gap-spacing-75 mb-spacing-50">
 						<span class="text-icon-success text-base leading-none">&#x2714;</span>
 						<strong class="heading-sm">Do</strong>
@@ -19,7 +19,7 @@ export function doDont(
 			<div class="flex-1 rounded-lg overflow-hidden flex flex-col">
 				<div class="bg-neutral-100 p-spacing-300 flex items-center justify-center flex-1">${dontExample}</div>
 				<div class="border-t-[3px] border-danger"></div>
-				<div class="bg-danger-default p-spacing-200">
+				<div class="bg-danger-default p-spacing-200 flex-1">
 					<div class="flex items-center gap-spacing-75 mb-spacing-50">
 						<span class="text-icon-danger text-base leading-none">&#x2716;</span>
 						<strong class="heading-sm">Don't</strong>
