cc69d8e32ecdd0098655c35fe80f38f495eaac79
Gerard <gerard.lamusse@bcc.no>
2026-04-13 13:54:15 +0200

    Fix message box size

--- component-library/src/components/wrapped/BccMessage.vue
+++ component-library/src/components/wrapped/BccMessage.vue
@@ -79,7 +79,7 @@ const iconWrapperStyles = computed(() => {
 		<slot>
 			<div>
 				<h4 v-if="title" class="text-heading-sm leading-lg">{{ title }}</h4>
-				<p v-if="message" class="text-body-sm">{{ message }}</p>
+				<p v-if="message" class="text-body-md">{{ message }}</p>
 			</div>
 		</slot>
 	</BccMessage>
