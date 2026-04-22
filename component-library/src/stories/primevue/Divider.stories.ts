14e920c4d161bd663863255ee60832a6c1c9fe29
Jan-Sverre Riksfjord <jan.sverre.riksfjord@bcc.no>
2026-04-22 10:57:49 +0000

    update story to make it look somewhat better

--- component-library/src/stories/primevue/Divider.stories.ts
+++ component-library/src/stories/primevue/Divider.stories.ts
@@ -23,7 +23,7 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Horizontal: Story = {
-	args: { layout: 'horizontal' },
+	args: { layout: 'horizontal', class: 'py-3' },
 	render: args => ({
 		components: { BccDivider },
 		setup() {
