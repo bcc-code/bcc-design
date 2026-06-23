c08ced96eb6773731cb2d3a8babf0a9c9d468f7f
Jan-Sverre Riksfjord <jan.sverre.riksfjord@bcc.no>
2026-06-23 15:31:23 +0200

    feat/data bcc name (#380)

--- component-library/vite.config.ts
+++ component-library/vite.config.ts
@@ -3,11 +3,12 @@ import vue from '@vitejs/plugin-vue';
 import { fileURLToPath } from 'node:url';
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
+import { componentDataAttrPlugin } from './vite-plugin-component-attr';
 
 const __dirname = fileURLToPath(new URL('.', import.meta.url));
 
 export default defineConfig({
-	plugins: [vue(), tailwindcss()],
+	plugins: [componentDataAttrPlugin(), vue(), tailwindcss()],
 	resolve: {
 		alias: {
 			'@': resolve(__dirname, './src'),
